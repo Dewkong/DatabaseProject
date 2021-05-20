acd2ee79bb7d99ef4e15f44bc1d5afe0b122ed0f
TheVoidek <wojtek.piechaczek@gmail.com>
2021-05-20 12:32:33 +0200

    Very basic tour information display

--- src/main/java/agh/cs/projekt/ui/FXMLTourDetailsController.java
+++ src/main/java/agh/cs/projekt/ui/FXMLTourDetailsController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 
 public class FXMLTourDetailsController implements Initializable {
@@ -18,11 +19,15 @@ public class FXMLTourDetailsController implements Initializable {
     public ImageView image_view;
     @FXML
     public Label tour_country;
+    @FXML
+    public Label tour_description;
+    @FXML
+    public Label tour_date;
+    @FXML
+    public Label tour_price;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-        //TODO
 
     }
 
@@ -31,6 +36,11 @@ public class FXMLTourDetailsController implements Initializable {
         ImageController.loadFromSource(image_view, tour.getImage());
         tour_name.setText(tour.getName());
         tour_country.setText(tour.getCountry().toString());
+        tour_description.setText(tour.getDescription());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        tour_date.setText(sdf.format(tour.getTourDate()));
+        tour_price.setText(String.format("%.2f", tour.getPrice()) + "z\u0142"); // \u0142 - unicode for ł
+
 
     }
 
