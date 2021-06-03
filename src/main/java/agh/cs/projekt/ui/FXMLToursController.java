ebd10607303289603845982a2a8a9cb67fd321d6
Dewkong <kryniu113@gmail.com>
2021-06-03 18:36:20 +0200

    Multithread in tours; filters alfa

--- src/main/java/agh/cs/projekt/ui/FXMLToursController.java
+++ src/main/java/agh/cs/projekt/ui/FXMLToursController.java
@@ -1,21 +1,23 @@
 package agh.cs.projekt.ui;
 
-import agh.cs.projekt.models.ApplicationUser;
-import agh.cs.projekt.models.RoleEnum;
-import agh.cs.projekt.models.Tour;
+import agh.cs.projekt.models.*;
 import agh.cs.projekt.services.DatabaseHolder;
 import agh.cs.projekt.services.NavigationService;
 import agh.cs.projekt.services.UserHolder;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import javax.persistence.Query;
 import java.io.IOException;
@@ -36,6 +38,14 @@ public class FXMLToursController implements Initializable {
     private GridPane gridMain;
     @FXML
     private Button addButton;
+    @FXML
+    private Button testButton;
+    @FXML
+    private ChoiceBox<CountryEnum> countryBox;
+
+    //cache
+    List<Tour> tours = null;
+    GridPane gridTours;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -47,79 +57,88 @@ public class FXMLToursController implements Initializable {
         logoutButton.setText("Wyloguj");
         addButton.setText("Dodaj wycieczke");
         addButton.setVisible(user.getRole() == RoleEnum.ADMIN);
+        testButton.setText("Test filtr");
 
-        DatabaseHolder databaseHolder = DatabaseHolder.getInstance();
-        try (Session session = databaseHolder.getSession()) {
-            session.beginTransaction();
-            Query query_tours = session.createQuery("select T from Tour as T");
-            @SuppressWarnings("unchecked") List<Tour> tours = query_tours.getResultList();
-            session.getTransaction().commit();
+        gridTours = new GridPane();
+        gridTours.setAlignment(Pos.TOP_CENTER);
+        gridTours.setStyle("-fx-border-insets: 5px");
+        gridTours.setStyle("-fx-padding: 5px");
+        gridTours.setStyle("-fx-background-insets: 5px");
+        gridTours.setVgap(10);
+        gridTours.setHgap(10);
+        gridMain.add(gridTours, 0, 2);
 
-            GridPane gridTours = new GridPane();
-            gridTours.setAlignment(Pos.CENTER);
-            gridTours.setStyle("-fx-border-insets: 5px");
-            gridTours.setStyle("-fx-padding: 5px");
-            gridTours.setStyle("-fx-background-insets: 5px");
-            gridTours.setVgap(10);
-            gridTours.setHgap(10);
+        DatabaseHolder.getInstance().dbCallNonBlocking(
+                session -> {
+                    session.beginTransaction();
+                    Query query_tours = session.createQuery("select T from Tour as T");
+                    @SuppressWarnings("unchecked") List<Tour> result = query_tours.getResultList();
+                    tours = result;
+                    session.getTransaction().commit();
+                },
+                () -> Platform.runLater(this::loadToursUI)
+        );
+    }
 
-            int i = 0;
-            for (Tour tour : tours){
-                GridPane gridTour = new GridPane();
-                gridTour.setAlignment(Pos.CENTER);
-                gridTour.setStyle("-fx-background-color: teal");
-                gridTour.setPadding(new Insets(10, 10, 10, 10));
+    public void loadToursUI(){
+        int i = 0;
+        for (Tour tour : tours) {
+            GridPane gridTour = new GridPane();
+            gridTour.setAlignment(Pos.CENTER);
+            gridTour.setStyle("-fx-background-color: teal");
+            gridTour.setPadding(new Insets(10, 10, 10, 10));
 
-                Label labelName = new Label(tour.getName() + " : " + tour.getCountry());
-                labelName.setAlignment(Pos.CENTER);
-                labelName.setMinWidth(180);
-                labelName.setMaxWidth(180);
-                labelName.setMinHeight(40);
-                Label labelDate = new Label("Data: " + tour.getTourDate().toString());
-                labelDate.setAlignment(Pos.CENTER);
-                labelDate.setMinWidth(180);
-                labelDate.setMaxWidth(180);
-                labelDate.setMinHeight(40);
-                Label labelPrice = new Label("Cena: " + tour.getPrice());
-                labelPrice.setAlignment(Pos.CENTER);
-                labelPrice.setMinWidth(180);
-                labelPrice.setMaxWidth(180);
-                labelPrice.setMinHeight(40);
-                Label labelPlaces = new Label();
-                labelPlaces.setAlignment(Pos.CENTER);
-                labelPlaces.setMinWidth(180);
-                labelPlaces.setMaxWidth(180);
-                labelPlaces.setMinHeight(40);
-                try (Session session2 = databaseHolder.getSession()) {
-                    session2.beginTransaction();
-                    Query query_available = session2.createNativeQuery("select available_places(:queried_id) from dual").setParameter("queried_id", tour.getId());
-                    @SuppressWarnings("unchecked") BigDecimal available = (BigDecimal) query_available.getSingleResult();
-                    session2.getTransaction().commit();
-                    labelPlaces.setText("Wolne miejsca: " + available + "/" + tour.getMaxPlaces());
-                }
+            Label labelName = new Label(tour.getName() + " : " + tour.getCountry());
+            labelName.setAlignment(Pos.CENTER);
+            labelName.setMinWidth(180);
+            labelName.setMaxWidth(180);
+            labelName.setMinHeight(40);
+            Label labelDate = new Label("Data: " + tour.getTourDate().toString());
+            labelDate.setAlignment(Pos.CENTER);
+            labelDate.setMinWidth(180);
+            labelDate.setMaxWidth(180);
+            labelDate.setMinHeight(40);
+            Label labelPrice = new Label("Cena: " + tour.getPrice());
+            labelPrice.setAlignment(Pos.CENTER);
+            labelPrice.setMinWidth(180);
+            labelPrice.setMaxWidth(180);
+            labelPrice.setMinHeight(40);
+            Label labelPlaces = new Label();
+            labelPlaces.setAlignment(Pos.CENTER);
+            labelPlaces.setMinWidth(180);
+            labelPlaces.setMaxWidth(180);
+            labelPlaces.setMinHeight(40);
+            labelPlaces.setText("Wolne miejsca: " + tour.getAvailablePlaces() + "/" + tour.getMaxPlaces());
 
-                Button detailsButton = new Button("Wiecej");
-                detailsButton.setOnAction(e -> {
-                    NavigationService.getInstance().setScene(
-                            "tour_details_scene.fxml",
-                            (FXMLTourDetailsController controller) ->{
-                                controller.displayTour(tour);
-                            });
-                });
+            Button detailsButton = new Button("Więcej");
+            detailsButton.setOnAction(e -> {
+                NavigationService.getInstance().setScene(
+                        "tour_details_scene.fxml",
+                        (FXMLTourDetailsController controller) -> {
+                            controller.displayTour(tour);
+                        });
+            });
 
-                HBox detailsBox = new HBox(detailsButton);
-                detailsBox.setAlignment(Pos.CENTER);
+            HBox detailsBox = new HBox(detailsButton);
+            detailsBox.setAlignment(Pos.CENTER);
 
-                gridTour.add(labelName, 0, 0);
-                gridTour.add(labelPrice, 0, 1);
-                gridTour.add(labelDate, 0, 2);
-                gridTour.add(labelPlaces, 0, 3);
-                gridTour.add(detailsBox, 0, 4);
-                gridTours.add(gridTour, i % 3, i / 3);
-                i++;
+            gridTour.add(labelName, 0, 0);
+            gridTour.add(labelPrice, 0, 1);
+            gridTour.add(labelDate, 0, 2);
+            gridTour.add(labelPlaces, 0, 3);
+            gridTour.add(detailsBox, 0, 4);
+            gridTours.add(gridTour, i % 3, i / 3);
+            i++;
+
+            if (!countryBox.getItems().contains(tour.getCountry())){
+                countryBox.getItems().add(tour.getCountry());
             }
-            gridMain.add(gridTours, 0, 1);
         }
+    }
+
+    public void refreshToursUI(){
+        gridTours.getChildren().removeAll(gridTours.getChildren());
+        Platform.runLater(this::loadToursUI);
     }
 
     public void logout(ActionEvent actionEvent) {
@@ -128,6 +147,29 @@ public class FXMLToursController implements Initializable {
 
         NavigationService.getInstance().flushHistory();
         NavigationService.getInstance().setScene("login_scene.fxml");
+    }
+
+    public void testTour(ActionEvent actionEvent){
+        tours.removeIf(tour -> tour.getCountry() == CountryEnum.CANADA);
+        refreshToursUI();
+    }
+
+    public void filterCountries(){
+        tours.removeIf(tour -> tour.getCountry() != countryBox.getValue());
+        refreshToursUI();
+    }
+
+    public void countryChosen(ActionEvent actionEvent){
+        System.out.println(countryBox.getValue());
+        DatabaseHolder.getInstance().dbCallNonBlocking(
+                session -> {
+                    session.beginTransaction();
+                    Query query_tours = session.createQuery("select T from Tour as T");
+                    @SuppressWarnings("unchecked") List<Tour> result = query_tours.getResultList();
+                    tours = result;
+                    session.getTransaction().commit();
+                },
+                () -> Platform.runLater(this::filterCountries));
     }
 
     public void addTour(ActionEvent actionEvent) throws IOException {
